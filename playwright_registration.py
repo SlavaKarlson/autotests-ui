b2d022f66850916d86ffdfca41852b809d7e0834
SlavaKarlson <slavakarlson@gmail.com>
2026-01-04 13:19:00 +0300

    Введение в Playwright. Практика с Playwright - fixes

--- playwright_registration.py
+++ playwright_registration.py
@@ -5,25 +5,24 @@ with sync_playwright() as playwright:
     page = browser.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
     email_input = page.get_by_test_id("registration-form-email-input").locator('input')
-    expect(email_input).to_be_visible()
     email_input.fill("user.name@gmail.com")
 
     username_input = page.get_by_test_id("registration-form-username-input").locator('input')
-    expect(username_input).to_be_visible()
     username_input.fill("username")
 
-
     password_input = page.get_by_test_id("registration-form-password-input").locator('input')
-    expect(password_input).to_be_visible()
     password_input.fill("password")
 
     registration_button = page.get_by_test_id("registration-page-registration-button")
-    expect(registration_button).to_be_visible()
     registration_button.click()
 
-    dashboard_header = page.get_by_test_id("dashboard-toolbar-title-text")
-    expect(dashboard_header).to_be_visible()
+    dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text("Dashboard")
+
+
 
 
 
